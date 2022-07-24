b8fa3b810dd2bd971d840b9bb08c62a0bbe6316e
박노경 <gerie99@naver.com>
2022-07-24 17:37:13 +0900

    20220724_보드 vo 생성

--- SDSD_PJ/src/main/java/com/sdsd/mvc/ploboard/model/vo/ploboard.java
+++ SDSD_PJ/src/main/java/com/sdsd/mvc/ploboard/model/vo/ploboard.java
@@ -1,5 +1,25 @@
 package com.sdsd.mvc.ploboard.model.vo;
 
-public class ploboard {
+import java.util.Date;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class ploboard {
+	private int borId;
+	private int borWriterNo;
+	private String borWriterName;
+	private String borTitle;
+	private String borContent;
+	private int borNumber;
+	private Date createDate;
+	private Date correctDate;
+	private int readCount;
+	private String borStatus;
+	private String originalFile;
+	private String renamedFile;
 }
