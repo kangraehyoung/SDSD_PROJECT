8294b8314eb0e41b66dc58a1a5dda05f4ce9a9ca
A\A <jsunny1019@gmail.com>
2022-07-31 14:39:19 +0900

    no message

--- SDSD_PJ/src/main/java/com/sdsd/mvc/ploGroup/controller/GroupJoin.java
+++ SDSD_PJ/src/main/java/com/sdsd/mvc/ploGroup/controller/GroupJoin.java
@@ -7,35 +7,23 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-/**
- * Servlet implementation class GroupJoin
- */
+import com.sdsd.mvc.ploGroup.model.vo.PloGroup;
+
 @WebServlet("/plo/groupjoin")
 public class GroupJoin extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
     public GroupJoin() {
-        super();
-        // TODO Auto-generated constructor stub
     }
 
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
 	}
 
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
+		PloGroup plogroup = new PloGroup();
+		
+		plogroup.setPloNum(request.getParameter("plo_num"));
+		
 	}
 
 }
