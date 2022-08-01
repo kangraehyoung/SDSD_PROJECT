6bb6ba8341f949dce19cf3be304465867c65084a
강래형 <foguddl31@gmail.com>
2022-08-01 21:39:39 +0900

    20220801_댓글기능 구현

---
+++ SDSD_PJ/src/main/java/com/sdsd/mvc/indiboard/model/vo/Reply.java
@@ -0,0 +1,26 @@
+package com.sdsd.mvc.indiboard.model.vo;
+
+import java.util.Date;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Reply {
+	private int no;
+	
+	private int repboardNo;
+	
+	private int repwriterNo;
+	
+	private String repwriterId;
+	
+	private String repcontent;	
+	
+	private Date repcreateDate;
+	
+	private Date repmodifyDate;
+}
