37cfec52ae3b9facfb89c51cc71ba76c905e07ac
강래형 <foguddl31@gmail.com>
2022-08-01 20:03:35 +0900

    20220801_개인활동인증게시판 테이블변경 및 파일업로드 오류수정

--- SDSD_PJ/src/main/java/com/sdsd/mvc/indiboard/model/dao/BoardDao.java
+++ SDSD_PJ/src/main/java/com/sdsd/mvc/indiboard/model/dao/BoardDao.java
@@ -24,8 +24,7 @@ public class BoardDao {
 //						+ "FROM MY_ACT_BOARD M JOIN BOARD B ON (M.MA_WRITER_NUMBER = B.BOR_WRITER_NO) "
 //						+ "WHERE B.BOR_STATUS = 'Y'";
 		String query = "SELECT COUNT(*) "
-					+ "FROM MY_ACT_BOARD "
-					+ "WHERE MA_BOARD_ID = 1";
+					+ "FROM INDIBOARD ";
 		
 		try {
 			pstm = connection.prepareStatement(query);
@@ -48,30 +47,31 @@ public class BoardDao {
 		List<IndiBoard> indiboardlist = new ArrayList<>();
 		PreparedStatement pstm = null;;
 		ResultSet rs = null;
-		String query = "SELECT RNUM, MA_BOARD_NUMBER, MA_BOARD_TITLE, MEM_EMAIL, BOR_WRITER_NAME, CREATE_DATE, BOR_FILE, READCOUNT, BOR_STATUS "
-				+ " FROM ( "
+		String query = "SELECT *  "
+				+ "FROM ( "
 				+ "    SELECT ROWNUM AS RNUM, "
-				+ "       MA_BOARD_NUMBER, "
-				+ "       MA_BOARD_TITLE, "
-				+ "       MEM_EMAIL, "
-				+ "       BOR_WRITER_NAME, "
-				+ "       CREATE_DATE, "
-				+ "       BOR_FILE, "
-				+ "       READCOUNT, "
-				+ "       BOR_STATUS "
+				+ "           INDIBOR_NUMBER, "
+				+ "           INDIBOR_TITLE, "
+				+ "           MEM_EMAIL, "
+				+ "           INDIBOR_WRITER_NAME, "
+				+ "           INDI_CREATE_DATE, "
+				+ "           INDI_UPDATE_DATE, "
+				+ "           INDI_BOR_FILE, "
+				+ "           INDI_READCOUNT, "
+				+ "           INDI_BOR_STATUS "
 				+ "    FROM ( "
-				+ "        SELECT MA.MA_BOARD_NUMBER, "
-				+ "               MA.MA_BOARD_TITLE, "
-				+ "               M.MEM_EMAIL, "
-				+ "               B.BOR_WRITER_NAME, "
-				+ "               B.CREATE_DATE, "
-				+ "               B.BOR_FILE, "
-				+ "               B.READCOUNT, "
-				+ "               B.BOR_STATUS "
-				+ "         FROM MY_ACT_BOARD MA "
-				+ "         JOIN BOARD B ON (B.BOR_WRITER_NO = MA.MA_WRITER_NUMBER) "
-				+ "         JOIN MEMBER M ON (M.MEM_NUMBER = MA.MA_WRITER_NUMBER) "
-				+ "        ) "
+				+ "            SELECT IB.INDIBOR_NUMBER, "
+				+ "                   IB.INDIBOR_TITLE, "
+				+ "                   M.MEM_EMAIL, "
+				+ "                   IB.INDIBOR_WRITER_NAME, "
+				+ "                   IB.INDI_CREATE_DATE, "
+				+ "                   IB.INDI_UPDATE_DATE, "
+				+ "                   IB.INDI_BOR_FILE, "
+				+ "                   IB.INDI_READCOUNT, "
+				+ "                   IB.INDI_BOR_STATUS "
+				+ "            FROM INDIBOARD IB "
+				+ "            JOIN MEMBER M ON (M.MEM_NUMBER = IB.INDIBOR_WRITER_NO) "
+				+ "    ) "
 				+ ") WHERE RNUM BETWEEN ? AND ?";
 		
 		
@@ -86,14 +86,14 @@ public class BoardDao {
 				IndiBoard indiBoard = new IndiBoard();
 				
 				indiBoard.setRowNum(rs.getInt("RNUM"));
-				indiBoard.setMaBorNo(rs.getInt("MA_BOARD_NUMBER"));
-				indiBoard.setBorTitle(rs.getString("MA_BOARD_TITLE"));
+				indiBoard.setMaBorNo(rs.getInt("INDIBOR_NUMBER"));
+				indiBoard.setBorTitle(rs.getString("INDIBOR_TITLE"));
 				indiBoard.setEmail(rs.getString("MEM_EMAIL"));
-				indiBoard.setWriterName(rs.getString("BOR_WRITER_NAME"));
-				indiBoard.setCreateDate(rs.getString("CREATE_DATE"));
-				indiBoard.setBorFile(rs.getString("BOR_FILE"));
-				indiBoard.setReadCount(rs.getInt("READCOUNT"));
-				indiBoard.setBorStatus(rs.getString("BOR_STATUS"));
+				indiBoard.setWriterName(rs.getString("INDIBOR_WRITER_NAME"));
+				indiBoard.setCreateDate(rs.getString("INDI_CREATE_DATE"));
+				indiBoard.setBorFile(rs.getString("INDI_BOR_FILE"));
+				indiBoard.setReadCount(rs.getInt("INDI_READCOUNT"));
+				indiBoard.setBorStatus(rs.getString("INDI_BOR_STATUS"));
 				
 				System.out.println(indiBoard.getEmail() + "에후");
 				System.out.println(indiBoard.getBorContent() + "에후후후");
@@ -116,7 +116,7 @@ public class BoardDao {
 		//int result2 = 0;
 		PreparedStatement pstmt = null;
 //		PreparedStatement pstmt2 = null;
-		String query_board = "INSERT INTO BOARD VALUES (SEQ_ALL_BOR_NUMBER.NEXTVAL, ?, ?, DEFAULT, DEFAULT, DEFAULT, ?, DEFAULT)";
+		String query_board = "INSERT INTO INDIBOARD VALUES(SEQ_INDIBOR_NUMBER.NEXTVAL, ?, ?, '제목넣어야함', ?, DEFAULT, DEFAULT, DEFAULT, ?, DEFAULT)";
 		//String query_indiBoard = "INSERT INTO MY_ACT_BOARD VALUES(SEQ_MY_BOR_NUM, 1, ?)";
 		
 		try {
@@ -125,10 +125,9 @@ public class BoardDao {
 			
 			pstmt.setInt(1, indiBoard.getWriterNo());
 			pstmt.setString(2, indiBoard.getWriterName());
-			pstmt.setString(3, indiBoard.getBorFile());
-			
-			//pstmt2.setInt(1, indiBoard.getWriterNo());
-			
+			pstmt.setString(3, indiBoard.getBorContent());
+			pstmt.setString(4, indiBoard.getBorFile());
+
 			result = pstmt.executeUpdate();
 			//result2 = pstmt2.executeUpdate();
 		} catch (SQLException e) {
@@ -141,25 +140,25 @@ public class BoardDao {
 		return result;
 	}
 
-	public int insertIndiBoard2(Connection connection, IndiBoard indiBoard) {
-		int result = 0;
-		PreparedStatement pstm = null;
-		String query = "INSERT INTO MY_ACT_BOARD VALUES(SEQ_MA_BOARD_NUMBER.NEXTVAL, 1, ?, '제목넣는공간', ?)";
-		// 제목은 jsp 파일 수정 후 넣어야합니다 
-		try {
-			pstm = connection.prepareStatement(query);
-			pstm.setInt(1, indiBoard.getWriterNo());
-			pstm.setString(2, indiBoard.getBorContent());
-
-			result = pstm.executeUpdate();
-		} catch (SQLException e) {
-			e.printStackTrace();
-		} finally {
-			close(pstm);
-		}
-		return result;
-		
-	}
+//	public int insertIndiBoard2(Connection connection, IndiBoard indiBoard) {
+//		int result = 0;
+//		PreparedStatement pstm = null;
+//		String query = "INSERT INTO MY_ACT_BOARD VALUES(SEQ_MA_BOARD_NUMBER.NEXTVAL, 1, ?, '제목넣는공간', ?, DEFAULT)";
+//		// 제목은 jsp 파일 수정 후 넣어야합니다 
+//		try {
+//			pstm = connection.prepareStatement(query);
+//			pstm.setInt(1, indiBoard.getWriterNo());
+//			pstm.setString(2, indiBoard.getBorContent());
+//
+//			result = pstm.executeUpdate();
+//		} catch (SQLException e) {
+//			e.printStackTrace();
+//		} finally {
+//			close(pstm);
+//		}
+//		return result;
+//		
+//	}
 
 	public int updateStatus(Connection connection, int maBorNo, String status) {
 		int result = 0;
@@ -187,18 +186,10 @@ public class BoardDao {
 		IndiBoard indiBoard = null;
 		PreparedStatement pstmt = null;
 		ResultSet rs = null;
-		String query = "SELECT MA.MA_BOARD_NUMBER, "
-				+ "       MA.MA_BOARD_TITLE, "
-				+ "       MA.MA_BOARD_CONTENT, "
-				+ "       M.MEM_NICKNAME, "
-				+ "       B.READCOUNT, "
-				+ "       B.BOR_FILE, "
-				+ "       B.CREATE_DATE, "
-				+ "       B.UPDATE_DATE "
-				+ "FROM MY_ACT_BOARD MA "
-				+ "JOIN MEMBER M ON(MA.MA_WRITER_NUMBER = M.MEM_NUMBER) "
-				+ "JOIN BOARD B ON(B.BOR_WRITER_NO = MA.MA_WRITER_NUMBER) "
-				+ "WHERE B.BOR_STATUS = 'Y' AND MA.MA_BOARD_NUMBER = ?";
+		String query = "SELECT * "
+					+ "FROM INDIBOARD IB "
+					+ "JOIN MEMBER M ON(IB.INDIBOR_WRITER_NO = M.MEM_NUMBER) "
+					+ "WHERE IB.INDI_BOR_STATUS = 'Y' AND INDIBOR_NUMBER = ?";
 		
 		try {
 			pstmt = connection.prepareStatement(query);
@@ -210,14 +201,14 @@ public class BoardDao {
 			while(rs.next()) {
 				indiBoard = new IndiBoard();
 				
-				indiBoard.setMaBorNo(rs.getInt("MA_BOARD_NUMBER"));
-				indiBoard.setBorTitle(rs.getString("MA_BOARD_TITLE"));
-				indiBoard.setBorContent(rs.getString("MA_BOARD_CONTENT"));
-				indiBoard.setWriterName(rs.getString("MEM_NICKNAME"));
-				indiBoard.setReadCount(rs.getInt("READCOUNT"));
-				indiBoard.setBorFile(rs.getString("BOR_FILE"));
-				indiBoard.setCreateDate(rs.getString("CREATE_DATE"));
-				indiBoard.setUpdateDate(rs.getString("UPDATE_DATE"));				
+				indiBoard.setMaBorNo(rs.getInt("INDIBOR_NUMBER"));
+				indiBoard.setBorTitle(rs.getString("INDIBOR_TITLE"));
+				indiBoard.setBorContent(rs.getString("INDIBOR_CONTENT"));
+				indiBoard.setWriterName(rs.getString("INDIBOR_WRITER_NAME"));
+				indiBoard.setReadCount(rs.getInt("INDI_READCOUNT"));
+				indiBoard.setBorFile(rs.getString("INDI_BOR_FILE"));
+				indiBoard.setCreateDate(rs.getString("INDI_CREATE_DATE"));
+				indiBoard.setUpdateDate(rs.getString("INDI_UPDATE_DATE"));				
 			}
 		} catch (SQLException e) {
 			e.printStackTrace();
--- SDSD_PJ/src/main/java/com/sdsd/mvc/indiboard/model/service/IndiBoardService.java
+++ SDSD_PJ/src/main/java/com/sdsd/mvc/indiboard/model/service/IndiBoardService.java
@@ -33,19 +33,19 @@ public class IndiBoardService {
 
 	public int save(IndiBoard indiBoard) {
 		int result = 0;
-		int result2 = 0;
+//		int result2 = 0;
 		Connection connection = getConnection();
 		
 		// 쿼리문 여러개 실행할때는 메소드 두개 만드는게 간편합니다!! service에서 result1,2 두개 받아서 처리만 하면 됩니다
 		
 		result = new BoardDao().insertIndiBoard(connection, indiBoard);
-		result2 = new BoardDao().insertIndiBoard2(connection, indiBoard);
+//		result2 = new BoardDao().insertIndiBoard2(connection, indiBoard);
 		
 		System.out.println("result" + result); // 참고용
-		System.out.println("result2" + result2); // 참고용
+//		System.out.println("result2" + result2); // 참고용
 		
 		// if문 처리
-		if(result > 0 && result2 > 0) {
+		if(result > 0) {
             commit(connection);
             
         } else {
