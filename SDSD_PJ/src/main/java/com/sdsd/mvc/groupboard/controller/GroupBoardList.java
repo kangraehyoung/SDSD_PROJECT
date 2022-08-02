3f853d9a5dbf02b8235411e71898491c9f6c2ef3
A\A <jsunny1019@gmail.com>
2022-08-02 10:12:53 +0900

    20220802_모임인증게시판 연결

--- SDSD_PJ/src/main/java/com/sdsd/mvc/groupboard/controller/GroupBoardList.java
+++ SDSD_PJ/src/main/java/com/sdsd/mvc/groupboard/controller/GroupBoardList.java
@@ -35,7 +35,7 @@ public class GroupBoardList extends HttpServlet {
     	
     	request.setAttribute("pageInfo", pageInfo);
     	request.setAttribute("groupBoardList", groupBoardList);
-    	request.getRequestDispatcher("").forward(request, response);
+    	request.getRequestDispatcher("http://localhost:8090/SDSD_PJ/views/ploboard/clubPlogBoard.jsp").forward(request, response);
     }
 
     @Override
